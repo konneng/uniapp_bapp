f2d4e24c11eff9efd01a5d97d01ca8ae83b84f8d
konneng <supplierservice@k-eng.it>
2025-08-01 07:33:25 +0200

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,54 +1,33 @@
-from fastapi import APIRouter, Depends
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from . import database, models, schemas
+from . import models, schemas
+from .database import get_db
 
 router = APIRouter()
 
-def get_db():
-    db = database.SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-# GET /buildings
-@router.get("/buildings", response_model=list[schemas.BuildingSchema])
-def get_buildings(db: Session = Depends(get_db)):
-    return db.query(models.Building).all()
-
-# POST /buildings
-@router.post("/buildings", response_model=schemas.BuildingSchema)
-def create_building(building: schemas.BuildingSchema, db: Session = Depends(get_db)):
-    db_building = models.Building(**building.dict())
-    db.add(db_building)
-    db.commit()
-    db.refresh(db_building)
-    return db_building
-
-# GET /zones
-@router.get("/zones", response_model=list[schemas.ZoneSchema])
-def get_zones(db: Session = Depends(get_db)):
-    return db.query(models.Zone).all()
-
-# POST /zones
-@router.post("/zones", response_model=schemas.ZoneSchema)
-def create_zone(zone: schemas.ZoneCreateSchema, db: Session = Depends(get_db)):
-    db_zone = models.Zone(**zone.dict())
+# Zone Types
+@router.post("/zone-types", response_model=schemas.ZoneTypeSchema)
+def create_zone_type(zone_type: schemas.ZoneTypeCreateSchema, db: Session = Depends(get_db)):
+    db_zone = models.ZoneType(name=zone_type.name)
     db.add(db_zone)
     db.commit()
     db.refresh(db_zone)
     return db_zone
 
-# GET /rooms
-@router.get("/rooms", response_model=list[schemas.RoomSchema])
-def get_rooms(db: Session = Depends(get_db)):
-    return db.query(models.Room).all()
+@router.get("/zone-types", response_model=list[schemas.ZoneTypeSchema])
+def get_zone_types(db: Session = Depends(get_db)):
+    return db.query(models.ZoneType).all()
 
-# POST /rooms
-@router.post("/rooms", response_model=schemas.RoomSchema)
-def create_room(room: schemas.RoomCreateSchema, db: Session = Depends(get_db)):
-    db_room = models.Room(**room.dict())
+# Room Types
+@router.post("/room-types", response_model=schemas.RoomTypeSchema)
+def create_room_type(room_type: schemas.RoomTypeCreateSchema, db: Session = Depends(get_db)):
+    db_room = models.RoomType(name=room_type.name)
     db.add(db_room)
     db.commit()
     db.refresh(db_room)
     return db_room
+
+@router.get("/room-types", response_model=list[schemas.RoomTypeSchema])
+def get_room_types(db: Session = Depends(get_db)):
+    return db.query(models.RoomType).all()
