c71269ed8b1609b4012922ce8d549c4a4bccbe36
konneng <supplierservice@k-eng.it>
2025-08-01 11:05:42 +0200

    Add files via upload

--- app/schemas.py
+++ app/schemas.py
@@ -1,106 +1,42 @@
+
 from pydantic import BaseModel
 from typing import Optional
 
-class ZoneTypeSchema(BaseModel):
-    id: int
+class ProtocolBase(BaseModel):
     name: str
+    description: Optional[str] = None
 
+class ProtocolCreateSchema(ProtocolBase):
+    pass
+
+class ProtocolSchema(ProtocolBase):
+    id: int
     class Config:
         orm_mode = True
 
-class ZoneTypeCreateSchema(BaseModel):
+class ObjectOfInterestCreateSchema(BaseModel):
     name: str
-
-class RoomTypeSchema(BaseModel):
-    id: int
-    name: str
-
-    class Config:
-        orm_mode = True
-
-class RoomTypeCreateSchema(BaseModel):
-    name: str
-
-class BuildingSchema(BaseModel):
-    id: str
-    country_code: str
-    province_code: str
-    name: str
-    address: str
-    latitude: Optional[float]
-    longitude: Optional[float]
-    building_type: str
-    group_id: str = "-1"
-
-    class Config:
-        orm_mode = True
-
-class ZoneSchema(BaseModel):
-    id: int
-    building_id: str
-    zone_type_id: int
-
-    class Config:
-        orm_mode = True
-
-class ZoneCreateSchema(BaseModel):
-    building_id: str
-    zone_type_id: int
-
-class RoomSchema(BaseModel):
-    id: int
-    zone_id: int
-    room_type_id: int
-
-    class Config:
-        orm_mode = True
-
-class RoomCreateSchema(BaseModel):
-    zone_id: int
-    room_type_id: int
-
-class POISchema(BaseModel):
-    id: int
-    room_id: int
-    name: str
-    status: str
-
-    class Config:
-        orm_mode = True
-
-class POICreateSchema(BaseModel):
-    room_id: int
-    name: str
-    status: str
-
-class EnvParamSchema(BaseModel):
-    id: int
-    room_id: int
-    temperature: Optional[float]
-    humidity: Optional[float]
-    co2_ppm: Optional[int]
-
-    class Config:
-        orm_mode = True
-
-class EnvParamCreateSchema(BaseModel):
-    room_id: int
-    temperature: Optional[float]
-    humidity: Optional[float]
-    co2_ppm: Optional[int]
-
-class DevicePresenceSchema(BaseModel):
-    id: int
-    room_id: int
-    device_id: str
     type: str
-    present: bool
+    visibility: str
+    room_id: int
+    owner_id: int
+    protocol_id: int
+    target_uri: Optional[str] = None
+    is_active: Optional[bool] = True
 
+class ObjectOfInterestSchema(ObjectOfInterestCreateSchema):
+    id: int
     class Config:
         orm_mode = True
 
-class DevicePresenceCreateSchema(BaseModel):
-    room_id: int
-    device_id: str
+class PointOfInterestCreateSchema(BaseModel):
+    name: str
     type: str
-    present: bool
+    visibility: str
+    room_id: int
+    owner_id: int
+
+class PointOfInterestSchema(PointOfInterestCreateSchema):
+    id: int
+    class Config:
+        orm_mode = True
