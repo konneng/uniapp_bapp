e07c57585cc4575e7d8ae32639a37e10b1ec5d8c
konneng <supplierservice@k-eng.it>
2025-08-01 11:09:07 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from . import models, database
 from .routes import router
+from .routes_protocols import router as protocols_router  # ✅ nuovo import
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 app.include_router(router)
+app.include_router(protocols_router)  # ✅ nuovo router
